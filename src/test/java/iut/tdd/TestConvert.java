0ab24c805f728ae03d63171be4932117c9f9faf5
Mamadou <uodamam@hotmail.com>
2015-04-17 15:06:05 +0200

    Adding Testing

--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -8,13 +8,13 @@ public class TestConvert {
 	@Test
 	public void test_num2text_zero () {
 		// Given
-		String input = "0";
+		//String input = "0";
 		String expected = "zéro";
-		Convert myConvert = new Convert();
+		//Convert myConvert = new Convert();
 		// When
-		String actual = myConvert.num2text(input);
+		//String actual = myConvert.num2text(input);
 		// Then
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals(expected, new Convert().num2text("0"));
 	}
 	@Test
 	public void test_num3text_un(){
