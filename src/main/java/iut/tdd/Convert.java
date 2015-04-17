6726c13daf12e6c094771f876addcb178e0e5206
Mamadou <uodamam@hotmail.com>
2015-04-17 14:45:51 +0200

    Update

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -2,7 +2,16 @@ package iut.tdd;
 
 public class Convert {
 	public static String num2text(String input) {
-		return "zero";
+		int taille = input.length() ; 
+		
+		switch(input){
+		case "0": return "zero" ;  
+		case "1": return "Un" ;  
+		case "2": return "Deux" ;  
+		case "3": return "Trois" ;  
+		default:  return "Default"  ;    
+		
+		}
 	}
 	public static String text2num(String input) {
 		return "zero";
