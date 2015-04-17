0f614b0e450f6628c53cba354aade87490468c1f
Mamadou <uodamam@hotmail.com>
2015-04-17 15:23:07 +0200

    Adding already

--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -14,18 +14,54 @@ public class TestConvert {
 		// When
 		//String actual = myConvert.num2text(input);
 		// Then
-		Assert.assertEquals(expected, new Convert().num2text("0"));
+		Assert.assertEquals(expected,  Convert.num2text("0"));
 	}
 	@Test
-	public void test_num3text_un(){
+	public void test_num3text_onze(){
 		
-		Assert.assertEquals("11", new Convert().text2num("Onze"));
+		Assert.assertEquals("11",  Convert.num2text("Onze"));
 		
 	
 	}
 	@Test
-	public void test_num3text_deux(){
-		Assert.assertEquals("13", new Convert().num2text("Treize"));
+	public void test_num3text_treize(){
+		Assert.assertEquals("13",  Convert.num2text("Treize"));
+	}
+	@Test
+	public void test_num3text_quatre(){
+		Assert.assertEquals("4",  Convert.num2text("Quatre"));
+	}
+	@Test
+	public void test_num3text_cinq(){
+		Assert.assertEquals("5",  Convert.num2text("Cinq"));
+	}
+	@Test
+	public void test_num3text_six(){
+		Assert.assertEquals("6",  Convert.num2text("Six"));
+	}
+	@Test
+	public void test_num3text_sept(){
+		Assert.assertEquals("7",  Convert.num2text("Sept"));
+	}
+	@Test
+	public void test_num3text_huit(){
+		Assert.assertEquals("8",  Convert.num2text("Huit"));
+	}
+	@Test
+	public void test_num3text_neuf(){
+		Assert.assertEquals("9",  Convert.num2text("neuf"));
+	}
+	@Test
+	public void test_num3text_dix(){
+		Assert.assertEquals("10",  Convert.num2text("Dix"));
+	}
+	@Test
+	public void test_num3text_douze(){
+		Assert.assertEquals("12",  Convert.num2text("Douze"));
+	}
+	@Test
+	public void test_num3text_quinze(){
+		Assert.assertEquals("15",  Convert.num2text("Quinze"));
 	}
 	
 }
